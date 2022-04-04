cb189aa6fdba61fe2646acb7bced1acbc95a47b5
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2022-04-04 14:46:11 +0200

    update hash table and LSH functionality

--- assignment_1/main.py
+++ assignment_1/main.py
@@ -10,6 +10,46 @@ def create_random_matrix(n, d):
     return np.random.default_rng().choice([+1, 0, -1], size=(n, d), p=[1 / 6, 2 / 3, 1 / 6]) * sqrt(3)
 
 
+class HashTable:
+    def __init__(self, hash_size, inp_dimensions):
+        self.hash_size = hash_size
+        self.inp_dimensions = inp_dimensions
+        self.hash_table = dict()
+        self.projections = create_random_matrix(self.hash_size, inp_dimensions)
+
+    def generate_hash(self, inp_vector):
+        bools = (np.dot(inp_vector, self.projections.T) > 0).astype('int')
+        return ''.join(bools.astype('str'))
+
+    def __setitem__(self, inp_vec, label):
+        hash_value = self.generate_hash(inp_vec)
+        self.hash_table[hash_value] = self.hash_table.get(hash_value, list()) + [label]
+
+    def __getitem__(self, inp_vec):
+        hash_value = self.generate_hash(inp_vec)
+        return self.hash_table.get(hash_value, [])
+
+
+class LSH:
+    def __init__(self, num_tables, hash_size, inp_dimensions):
+        self.num_tables = num_tables
+        self.hash_size = hash_size
+        self.inp_dimensions = inp_dimensions
+        self.hash_tables = list()
+        for i in range(self.num_tables):
+            self.hash_tables.append(HashTable(self.hash_size, self.inp_dimensions))
+
+    def __setitem__(self, inp_vec, label):
+        for table in self.hash_tables:
+            table[inp_vec] = label
+
+    def __getitem__(self, inp_vec):
+        results = list()
+        for table in self.hash_tables:
+            results.extend(table[inp_vec])
+        return list(set(results))
+
+
 # import and filter data
 tracks = pd.read_csv("fma_metadata/tracks.csv", index_col=0, header=[0, 1])
 tracks = tracks[tracks["set"]["subset"] == "small"]
