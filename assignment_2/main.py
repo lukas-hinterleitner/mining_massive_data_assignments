8a22194f06d7a974abb7ee8380c477a8ec3c8008
Lukas Hinterleitner <lukas.hinterleitner98@gmail.com>
2022-05-03 20:41:02 +0200

    update svm

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 
 from os.path import exists
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
 
+np.random.seed(42)
 
 def import_data(path: str):
     """
@@ -28,7 +31,8 @@ def import_data(path: str):
         elif file_extension == "npz":
             data = np.load(path)
 
-            X_train, y_train, X_test, y_test = data["train"].T, data["train_labels"].T, data["test"].T, data["test_labels"].T
+            X_train, y_train, X_test, y_test = data["train"].T, data["train_labels"].T, data["test"].T, data[
+                "test_labels"].T
 
             print(f"X train shape: {X_train.shape}")
             print(f"y train shape: {y_train.shape}")
@@ -42,33 +46,52 @@ def import_data(path: str):
         raise FileNotFoundError("file not found")
 
 
-def predict(x, w):
-    return w.T.dot(x)
+class CustomSVM:
+    def __init__(self, learning_rate=0.001, epochs=100, C=1):
+        self.w_star = None
+        self.learning_rate = learning_rate
+        self.epochs = epochs
 
+        self.C = C
 
-class SVM:
-    def __init__(self, lambda_):
-        # hyperparameter
-        self.lambda_ = lambda_
-
-    # regularized hinge loss formulation
+    # good article: https://towardsdatascience.com/solving-svm-stochastic-gradient-descent-and-hinge-loss-8e8b4dd91f5b
     def fit(self, X, y):
-        def hinge_loss(x, y, w):
-            return np.max(0, 1 - y * predict(x, w))
-
-        def regularizer(w):
-            return self.lambda_ * np.linalg.norm(w)
-
         # TODO: implement multi class hinge loss
-        def objective(w):
-            return regularizer(w) + sum([hinge_loss(x, y_, w) for x, y_ in zip(X, y)])
+        def hinge_loss(x, y, w):  # return the loss and the corresponding gradient
+            loss = max(0, 1 - y * np.dot(w, x))
+
+            if loss == 0:
+                return loss, w
+            else:
+                return loss, w - self.C * y * x
+
+        # initialize w0 with zeros
+        w = np.zeros(X.shape[1])
+
+        # stochastic gradient descent
+        for epoch in tqdm(range(self.epochs)):
+            for _ in range(X.shape[0]):
+                random_idx = np.random.randint(0, X.shape[0])
+
+                x_random = X[random_idx]
+                y_random = y[random_idx]
+
+                loss, gradient = hinge_loss(x_random, y_random, w)
+
+                # perform weight update
+                w = w - self.learning_rate * gradient
+
+        self.w_star = w
 
     def predict(self, X):
-        pass
+        return np.sign(np.dot(X, self.w_star))
 
 
 toydata_small_X, toydata_small_y = import_data("data/toydata_tiny.csv")
 toydata_large_X, toydata_large_y = import_data("data/toydata_large.csv")
 mnist_X_train, mnist_y_train, mnist_X_test, mnist_y_test = import_data("data/mnist.npz")
 
-
+svm = CustomSVM()
+svm.fit(toydata_small_X, toydata_small_y)
+predictions = svm.predict(toydata_small_X)
+print(accuracy_score(toydata_small_y, predictions))
